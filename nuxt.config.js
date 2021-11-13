6da91cecaad0ebd3b3f8d6e2e95526ab8734ffde
lizhixuan <1031760551@qq.com>
2021-11-13 17:00:05 +0800

    【新增】额外路由添加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,14 @@
 export default {
+
+  // router配置
+  router: {
+    extendRoutes (routes, resolve) {
+      routes.push({
+        path: '/custom',
+        component: resolve(__dirname, 'pages/othername.vue')
+      })
+    }
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'nuxt',
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <template>
-  <div>李志轩</div>
+  <div>
+    <h2>商品列表</h2>
+    <ul>
+      <li v-for="good in goods" :key="good.id">
+        <nuxt-link :to="`/detail/${good.id}`">
+          <span>{{ good.text }}</span>
+          <span>￥{{ good.price }}</span>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      goods: [
+        { id: 1, text: 'html', price: 1000 },
+        { id: 2, text: 'css', price: 2000 },
+        { id: 3, text: 'js', price: 3000 }
+      ]
+    }
+  }
+}
 </script>
