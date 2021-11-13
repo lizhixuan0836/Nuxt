3b094608cb7a8300c97b8fb2a4d105d2264cdcde
lizhixuan <1031760551@qq.com>
2021-11-13 18:42:32 +0800

    【新增】自定义layout

---
+++ layouts/BaseLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/">
+        首页
+      </nuxt-link>
+      <nuxt-link to="/admin" no-prefetch>
+        管理
+      </nuxt-link>
+      <nuxt-link to="/cart">
+        购物车
+      </nuxt-link>
+    </nav>
+    <!-- page中定义的内容 -->
+    <Nuxt />
+  </div>
+</template>
